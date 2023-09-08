8133cd21e99ddd710593c4ffd88aa3ff21eabd96
everyones-guy <garywill64@gmail.com>
2023-09-08 17:17:35 -0500

    Library is working. API call is working. We have our data. Time to put it to use.

--- bin/FoodTruckApp.pl
+++ bin/FoodTruckApp.pl
@@ -1,11 +1,15 @@
 use strict;
 use warnings;
+use lib '../lib';
 use FoodTruck;
 use JSON;           
-use LWP::UserAgent; 
+use LWP::UserAgent;
 
-# Setup endpoint
-my $api_url = 'https://Whatever.com/api/foodtrucks'; #update with actual url
+use Data::Dumper;
+
+# Setup endpoint to grab json Data
+my $api_url = 'https://data.sfgov.org/resource/rqzj-sfat.json';
+
 
 # Create a User
 my $ua = LWP::UserAgent->new;
@@ -15,7 +19,7 @@ my $response = $ua->get($api_url);
 
 # Successful?
 if ($response->is_success) {
-    my $json_data = $response->decoded_content;
+    my $json_data = $response->decoded_content();
 
     # Grab the data and parse
     my $trucks = decode_json($json_data);
@@ -23,9 +27,25 @@ if ($response->is_success) {
     foreach my $truck_data (@$trucks) {
         my $truck = FoodTruck->new(%$truck_data);
 
-        # Test if we're displaying the correct data
-        print $truck->display_info();
-    }
+        # Test if we're displaying the correct data		
+		if( $truck->{'status'} =~ /approved/i){
+			say $truck->display_info() . "\n\n";
+		}
+	}	
 } else {
     die "Failed to retrieve data from the API: " . $response->status_line;
 }
+
+=pod
+We have our data, now it is time to do something fun with it. 
+
+We can set up a randomizer and have it query the end user to see what their preferences are and suggest food trucks that they may like. 
+
+We could use the food trucks' locations along with the end user's geo tag and find something close. 
+
+If we're actively looking for food, then we should only be looking at food trucks with approved statuses.
+
+If we want to see someone that is up and coming then we weill need to look for pending.
+
+If we want to see what happened to that old food truck we loved so much and is nowhere to be found, we'll need to know status and actively look around for any news.
+=cut
